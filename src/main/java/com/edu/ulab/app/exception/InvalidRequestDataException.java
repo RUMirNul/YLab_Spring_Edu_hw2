888795b1a1c5cefea572e364946f7f4ec8340506
lexacv01@gmail.com <Agent3228>
2022-09-26 19:28:51 +0300

    Added InvalidRequestDataException Added text error constants in ErrorMessageTextConstants Added handler in ControllerExceptionHandler

---
+++ src/main/java/com/edu/ulab/app/exception/InvalidRequestDataException.java
@@ -0,0 +1,7 @@
+package com.edu.ulab.app.exception;
+
+public class InvalidRequestDataException extends RuntimeException{
+    public InvalidRequestDataException(String message) {
+        super(message);
+    }
+}
