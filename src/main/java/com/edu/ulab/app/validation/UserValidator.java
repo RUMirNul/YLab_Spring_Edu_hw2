cdd1333f47b584f09e3e99af6bde695235fdfb30
lexacv01@gmail.com <Agent3228>
2022-09-26 19:30:20 +0300

    Added validation for users and books

---
+++ src/main/java/com/edu/ulab/app/validation/UserValidator.java
@@ -0,0 +1,32 @@
+package com.edu.ulab.app.validation;
+
+import com.edu.ulab.app.dto.UserDto;
+
+public class UserValidator {
+
+    /**
+     * Validations of BookDto fields.
+     * Fields fullName and title checked for null and blank, age checked for positive number not exceeding 120.
+     * @param userDto object for validation.
+     * @return boolean is valid object.
+     */
+    public static boolean isValidUserData(UserDto userDto) {
+        boolean isValidFullName = false;
+        boolean isValidTitle = false;
+        boolean isValidAge = false;
+
+        if (userDto.getFullName() != null && !userDto.getFullName().isBlank()) {
+            isValidFullName = true;
+        }
+
+        if (userDto.getTitle() != null && !userDto.getTitle().isBlank()) {
+            isValidTitle = true;
+        }
+
+        if (userDto.getAge() > 0 && userDto.getAge() < 120) {
+            isValidAge = true;
+        }
+
+        return isValidFullName && isValidTitle && isValidAge;
+    }
+}
