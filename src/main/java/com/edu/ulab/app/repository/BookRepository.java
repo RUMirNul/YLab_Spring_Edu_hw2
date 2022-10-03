9432a08c58bb1e5ccd63288b21edccd8f74e3f79
lexacv01@gmail.com <Agent3228>
2022-10-03 23:48:56 +0300

    Changed Query

--- src/main/java/com/edu/ulab/app/repository/BookRepository.java
+++ src/main/java/com/edu/ulab/app/repository/BookRepository.java
@@ -1,12 +1,12 @@
 package com.edu.ulab.app.repository;
 
 import com.edu.ulab.app.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("select b.id from Book b where b.person.id = :userId")
     List<Long> findAllBooksIdByUserId(long userId);
